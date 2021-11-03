375478d31b5edf77f60ce3396d0853ce047609dc
Zach Perrault <zach.perrault@gmail.com>
2021-11-03 10:36:53 -0400

    feat: solution generator

--- 2021/lib/mix/advent_of_code.ex
+++ 2021/lib/mix/advent_of_code.ex
@@ -2,4 +2,12 @@ defmodule Mix.AdventOfCode do
   @moduledoc """
   Helpers for `AdventOfCode` mix tasks.
   """
+
+  def day_module(day) do
+    Module.concat(AdventOfCode, Macro.camelize("Day#{day}"))
+  end
+
+  def part_module(day, part) do
+    Module.concat(day_module(day), Macro.camelize("Part#{part}"))
+  end
 end
