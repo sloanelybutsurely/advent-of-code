5408efc53628e16a65161dbe18e46bbfb20b5394
Sloane Perrault <sloane.perrault@gmail.com>
2022-12-01 08:57:41 -0500

    solve 2022 day 1 part 2

--- 2022/app/Main.hs
+++ 2022/app/Main.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
+
+import Data.List (sort)
+import Data.Text (Text, pack, splitOn, unpack)
     
 
 part1 :: String -> Integer
@@ -9,9 +13,19 @@ part1 str = findMax lines' [] 0
     findMax ("":xs) acc cMax = findMax xs [] $ max (sum acc) cMax
     findMax (x:xs) acc cMax = findMax xs ((read x):acc) cMax
 
+chunkElves :: Text -> [Integer]
+chunkElves t = integers
+  where
+    textChunks = splitOn "\n\n" t
+    integerChunks = map (map read . words . unpack) textChunks
+    integers = map sum integerChunks
+
+part2 :: String -> Integer 
+part2 str = sum $ take 3 $ reverse $ sort $ chunkElves $ pack str
+
 main :: IO ()
 main = do
   contents <- getContents
 
-  putStrLn $ show $ part1 contents
+  putStrLn $ show $ part2 contents
 
