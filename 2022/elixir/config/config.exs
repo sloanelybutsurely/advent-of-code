12ce2934eaf280a53c8d7feb390c5df2790dc87c
Sloane Perrault <sloane.perrault@gmail.com>
2022-12-10 09:49:01 -0500

    more 2022 elixir setup

---
+++ 2022/elixir/config/config.exs
@@ -0,0 +1,5 @@
+import Config
+
+config :advent_of_code_utils,
+  auto_compile?: true,
+  session: System.get_env("ADVENT_OF_CODE_SESSION")
--- 2022/elixir/test/aoc2022_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Aoc2022Test do
-  use ExUnit.Case
-  doctest Aoc2022
-
-  test "greets the world" do
-    assert Aoc2022.hello() == :world
-  end
-end
