1e9c2a645d6a342e1ff704e48672f0664597d9ce
Sloane Perrault <sloane.perrault@gmail.com>
2022-12-10 09:26:54 -0500

    setup elixir project

---
+++ 2022/elixir/lib/aoc2022.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2022 do
+  @moduledoc """
+  Documentation for `Aoc2022`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2022.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2022/elixir/test/aoc2022_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2022Test do
+  use ExUnit.Case
+  doctest Aoc2022
+
+  test "greets the world" do
+    assert Aoc2022.hello() == :world
+  end
+end
