fc7ae556c125cd7d4b9562312822fcfce6f84933
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2023-12-04 08:54:37 -0500

    solve 2023 4.2

--- 2023/elixir/lib/2023/4.ex
+++ 2023/elixir/lib/2023/4.ex
@@ -9,11 +9,40 @@ aoc 2023, 4 do
     |> Enum.sum()
   end
 
-  def p2(_input) do
-    # input
-    # |> String.split("\n")
-    # |> Enum.map(&parse_line/1)
-    # |> Enum.map(&score_2/1)
+  def p2(input) do
+    cards =
+      input
+      |> String.split("\n")
+      |> Enum.map(&parse_line/1)
+      |> Enum.map(&score_2/1)
+
+    card_counts =
+      cards
+      |> Stream.map(fn {id, _} -> {id, 1} end)
+      |> Enum.into(%{})
+
+    cards
+    |> Enum.reduce(card_counts, fn {id, winners}, counts ->
+      if winners > 0 do
+        copies = Map.get(counts, id)
+
+        copied_range =
+          (id + 1)..(id + winners)
+
+        for copy_id <- copied_range, reduce: counts do
+          counts ->
+            if Map.has_key?(counts, copy_id) do
+              Map.update!(counts, copy_id, &(&1 + copies))
+            else
+              counts
+            end
+        end
+      else
+        counts
+      end
+    end)
+    |> Map.values()
+    |> Enum.sum()
   end
 
   def parse_line(line) do
