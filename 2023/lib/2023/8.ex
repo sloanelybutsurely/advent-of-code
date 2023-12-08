44c277ff94b615ab24d77b68108c2ff49a0aca9d
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2023-12-08 12:49:55 -0500

    solve 2023 8.2

--- 2023/lib/2023/8.ex
+++ 2023/lib/2023/8.ex
@@ -5,20 +5,40 @@ aoc 2023, 8 do
   def p1(input) do
     {instructions, mappings} = read_input(input)
 
-    start = "AAA"
-    finish = "ZZZ"
-
     all_but_last_step =
       instructions
       |> Stream.cycle()
-      |> Stream.scan(start, &step(&1, &2, mappings))
-      |> Stream.take_while(&(&1 != finish))
+      |> Stream.scan("AAA", &step(&1, &2, mappings))
+      |> Stream.take_while(&(&1 != "ZZZ"))
       |> Enum.count()
 
     all_but_last_step + 1
   end
 
-  def p2(_input) do
+  def p2(input) do
+    {instructions, mappings} = read_input(input)
+
+    starts =
+      mappings
+      |> Map.keys()
+      |> Enum.filter(&String.ends_with?(&1, "A"))
+
+    lengths =
+      starts
+      |> Enum.map(fn start ->
+        n =
+          instructions
+          |> Stream.cycle()
+          |> Stream.scan(start, &step(&1, &2, mappings))
+          |> Stream.take_while(&(not String.ends_with?(&1, "Z")))
+          |> Enum.count()
+
+        n + 1
+      end)
+
+    Enum.reduce(lengths, fn a, b ->
+      div(a * b, Integer.gcd(a, b))
+    end)
   end
 
   def step("L", curr, mappings) do
@@ -31,6 +51,10 @@ aoc 2023, 8 do
     mappings
     |> Map.get(curr)
     |> elem(1)
+  end
+
+  def step_all(inst, currs, mappings) do
+    Enum.map(currs, &step(inst, &1, mappings))
   end
 
   def read_input(input) do
